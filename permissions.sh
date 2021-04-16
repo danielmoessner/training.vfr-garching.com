4464685fd0a9fa27ffffe248be1c6a8f9d4a0eba
danielmoessner <github@moesibert.de>
2021-04-16 10:05:31 +0200

    updates

--- permissions.sh
+++ permissions.sh
@@ -1,9 +1,9 @@
-chown -R root:www-data /home/vfrgarching
-chmod -R 750 /home/vfrgarching
-find /home/vfrgarching -type f -print0|xargs -0 chmod 740
-chmod -R 770 /home/vfrgarching/tmp/media
-find /home/vfrgarching/tmp/media -type f -print0|xargs -0 chmod 760
-chmod 770 /home/vfrgarching/tmp/logs
-chmod -R 760 /home/vfrgarching/tmp/logs/*
-chmod 770 /home/vfrgarching/tmp
-chmod -R 760 /home/vfrgarching/tmp/db.sqlite3
+chown -R root:www-data /home/training.vfr-garching.com
+chmod -R 750 /home/training.vfr-garching.com
+find /home/training.vfr-garching.com -type f -print0|xargs -0 chmod 740
+chmod -R 770 /home/training.vfr-garching.com/tmp/media
+find /home/training.vfr-garching.com/tmp/media -type f -print0|xargs -0 chmod 760
+chmod 770 /home/training.vfr-garching.com/tmp/logs
+chmod -R 760 /home/training.vfr-garching.com/tmp/logs/*
+chmod 770 /home/training.vfr-garching.com/tmp
+chmod -R 760 /home/training.vfr-garching.com/tmp/db.sqlite3
