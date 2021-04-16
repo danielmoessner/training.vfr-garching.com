a877527c0b943866caccb6b934bf9b050d2cabd5
danielmoessner <github@moesibert.de>
2021-04-16 10:08:12 +0200

    updates

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 TODAY=`date +'%Y-%m-%d'`
-FILENAME="/home/vfrgarching/tmp/backup/${TODAY}.sqlite3"
-cp /home/vfrgarching/tmp/db.sqlite3 ${FILENAME}
+FILENAME="/home/training.vfr-garching.com/tmp/backup/${TODAY}.sqlite3"
+cp /home/training.vfr-garching.com/tmp/db.sqlite3 ${FILENAME}
 echo $FILENAME
